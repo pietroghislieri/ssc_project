121525aa5a5bb62dc850141258117bea353ea855
ghisli <pietro.ghislieri@studenti.unitn.it>
2018-11-07 12:28:07 +0100

    helloworld

---
+++ api.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var port = 3000;
+
+var requestHandler = function(request, response) {
+  console.log(request.url);
+  response.end('Hello World!');
+}
+
+var server = http.createServer(requestHandler);
+server.listen(port);
+console.log("Server started, listening on port", port);
