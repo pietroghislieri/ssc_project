fb1d193849e33b63c11dd9c9ead9cb9fa1011dc4
ghisli <pietro.ghislieri@studenti.unitn.it>
2018-11-07 13:47:28 +0100

    modify helloworld again

--- api.js
+++ api.js
@@ -1,9 +1,16 @@
-var http = require('http');
-var port = 3000;
-
-var requestHandler = function(request, response) {
-  console.log(request.url);
-  response.end('Hello World!');
-}
-
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+ 
+var courses_offered = [{id: 21, name: 'HCI'},
+{id: 28, name:'sweng'}]
+ 
+ 
+app.get('/', (req, res) => res.send('Hello World!'))
+ 
+app.get('/courses', (req, res) => {
+   res.json(courses_offered)
+})
+ 
+app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
 
