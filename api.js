ff6f572a70dcc5102cb27c7375a102a66c4d2a9a
Ghislieri Pietro (ACC0118359) <pietro.ghislieri@sci-a202pc24x.unitn.it>
2018-12-03 14:14:26 +0100

    modified api.js

--- api.js
+++ api.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const config = require('./config');
-const routes= require('./app/routes/home');
-const PORT = process.env.PORT || 3000;
+
 //const User   = require('./app/models/user');
 
 
@@ -23,17 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.set('superSecret', config.secret);
-
-routes(app);
-
-app.use('/home',routes);
-
-const server = app.listen(PORT, (error) => {
-  if (error) return console.log(`Error: ${error}`);
-
-  console.log(`Server listening on port ${server.address().port}`);
+app.get('/', function(req, res) {
+  res.send('Hello! The API is at /home');
 });
 
+
+var homeRoutes= require ('./app/routes/home')
+app.use('/api/home', homeRoutes);
+
 module.exports = app;
-/*
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));*/
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,15 +1,12 @@
 const express = require('express');
 var apiRoutes = express.Router();
 
-const router = apiRoutes => {
-	apiRoutes.get('/home', function(req, res) {
+
+apiRoutes.get('/', function(req, res) {
 		res.json({ message: 'Welcome to the coolest API on earth!' });
 	});
 
-	apiRoutes.get('/', function(req, res) {
-		res.send('Hello! The API is at /home');
-	});
-	
-}
 
-module.exports = router;
+
+
+module.exports = apiRoutes;
