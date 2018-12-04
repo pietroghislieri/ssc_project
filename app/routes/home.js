047d043bc5a22f87343842c4d90cc02d36324293
Leooliveri <leonardo.oliveri@studenti.unitn.it>
2018-12-04 15:04:39 +0100

    modified home

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,7 +3,7 @@ var apiRoutes = express.Router();
 
 
 apiRoutes.get('/', function(req, res) {
-		res.json([{ message: 'The exams are in /home/exams' },{message: 'The assignments are in /home/assignments'}]);
+		res.json([{ message: 'The exams are in /home/exams' },{message: 'The assignments are in /home/assignments'},{message: 'The students are in /home/students'},{message: 'The professors are in /home/professors'}]);
 	});
 
 
